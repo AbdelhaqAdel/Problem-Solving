5baed8e59a98e83cce509de6de73b86d995147eb
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2025-01-25 17:57:38 +0200

    add another solution

--- 67.add_binary/solution.dart
+++ 67.add_binary/solution.dart
@@ -1,69 +1,70 @@
 class Solution {
-String addBinary(String a, String b) {
-  String result = '';
-  int carry = 0;
-  int i = a.length - 1;
-  int j = b.length - 1;
+  //----------BEST SOLUTION---------------------
+// String addBinary(String a, String b) {
+  // String result = '';
+  // int carry = 0;
+  // int i = a.length - 1;
+  // int j = b.length - 1;
+  // while (i >= 0 || j >= 0 || carry > 0) {
+  //   int bitA = i >= 0 ? a[i].codeUnitAt(0) - '0'.codeUnitAt(0) : 0;
+  //   int bitB = j >= 0 ? b[j].codeUnitAt(0) - '0'.codeUnitAt(0) : 0;
+  //   int sum = bitA + bitB + carry;
+  //   result = (sum % 2).toString() + result;
+  //   carry = sum ~/ 2;
+  //   i--;
+  //   j--;
+  // }
 
-  while (i >= 0 || j >= 0 || carry > 0) {
-    int bitA = i >= 0 ? a[i].codeUnitAt(0) - '0'.codeUnitAt(0) : 0;
-    int bitB = j >= 0 ? b[j].codeUnitAt(0) - '0'.codeUnitAt(0) : 0;
-    int sum = bitA + bitB + carry;
-    result = (sum % 2).toString() + result;
-    carry = sum ~/ 2;
-    i--;
-    j--;
+  // return result;
+// }
+String addBinary(String a, String b) {
+
+
+    if(a.length < b.length){
+     a= addZeroes(a,b);
+    }else if(a.length > b.length){
+      b= addZeroes(b,a);
+    }
+      List<int> listA = a.split('').map((char) => int.parse(char)).toList();
+  List<int> listB = b.split('').map((char) => int.parse(char)).toList();
+
+    int carry=0;
+    String result='';
+    for (int i = a.length - 1; i >= 0; i--) {
+      if(listA[i]+listB[i]+carry>=2){       
+        if(listA[i]+listB[i]+carry==3){
+          result='1'+result;
+        }
+        else if(listA[i]+listB[i]+carry==2){
+          result='0'+result;          
+        }
+         carry =1;
+      }else{
+        result=(listA[i]+listB[i]+carry).toString()+result;
+        carry=0;
+      }
+    }
+    if(carry==1){
+      result='1'+result;
+    }
+    return result;
+
+}
+ String addZeroes(String a, String b) {
+      int length = b.length - a.length;
+
+    for (int i = 0; i <length; i++) {
+      a = '0' + a;
+    }
+    return a;
   }
 
-  return result;
-}
-
-
-    // if(a.length < b.length){
-    //  a= addZeroes(a,b);
-    // }else if(a.length > b.length){
-    //   b= addZeroes(b,a);
-    // }
-
-    // int carry = 0;
-    //   List<int> sum = [];
-    // for (int i = a.length - 1; i >= 0; i--) {
-    //   if(a[i]==1&&b[i]==1){
-    //     carry = 1;
-    //     sum.add(0);
-    //   }else{
-    //         sum.add(int.parse(a[i]) + int.parse(b[i]) + carry);
-    //   }
-     
-      // else if(a[i]==0&&b[i]==0){
-      //   carry = 0;
-      //   sum.add(0);
-      // }
-      // else {
-      //   carry = 0;
-      //   sum.add(1);
-      // }
-      
-      // sum.add(int.parse(a[i]) + int.parse(b[i]) + carry);
-      // //  sum. = int.parse(a[i]) + int.parse(b[i]) + carry;        
-      // carry = sum[i] ~/ 2;
-      // sum = sum % 2;
-    //}
-    // print(sum);
-    // return '';
-  
-
-//  String addZeroes(String a, String b) {
-//     for (int i = 0; i < b.length - a.length; i++) {
-//       a = '0' + a;
-//     }
-//     return a;
-//   }
 }
 
 void main() {
   Solution solution = Solution();
-  String a = "1010";
-  String b = "1011";
+  String a = "100";
+  String b = "110010";
   print(solution.addBinary(a, b));
 }
+
